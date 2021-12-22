2e2b702b3dfa04ffad522613eedd352acf25adde
rifkifikri <rifkifikri000@gmail.com>
2021-12-22 11:37:46 +0700

    revisi

--- CobaDatabase.php
+++ CobaDatabase.php
@@ -1,3 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
     $serverName = "localhost";
     $database = "db_siswa";
@@ -16,19 +26,18 @@ $connect = mysqli_connect($serverName, $username, $password, $database);
     $Hobby = $_POST['Hobi'];
     
     $query = mysqli_query ($connect, "INSERT INTO tb_siswa(nama_siswa,alamat_siswa,umur_siswa,nomor_hp,id_hobby) 
-    values('$nama','$alamat','$umur', '$umur','$Hobby')");
+    values('$nama','$alamat','$umur', '$nomor','$Hobby')");
     $sql = 'select * from tb_siswa';
-
+    
    // $tampil=mysqli_query($query,$sql);
-
-    echo $query;
+       echo $query;
         echo 'Nama Anda :'.$_POST["nama"].'<br>';
         echo 'Alamat :'.$_POST["alamat"].'<br>';
         echo 'Umur Anda : '.$_POST["umur"].'<br>';
         echo 'Nomor HP :'.$_POST["hp"].'<br>';
         echo 'Hobi Pelanggan : '.$_POST["Hobi"].'<br>';
         echo '<pre>'; print_r($_POST);// untuk menampilkan array yang di tampung
-   
+        
 // mengecek koneksi
 if (!$connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
@@ -38,4 +47,35 @@ if (!$connect) {
 }
 
 mysqli_close($connect);
+
 ?>
+
+
+<table border="1px">
+    <tr>    
+        <th>No</th>
+        <th> Id </th>
+        <th>Nama</th>
+        <th>Umur</th>
+        <th>Hobby</th>
+        <th>nomor HP</th>
+    </tr>
+
+    <?php
+        $no = 1;
+        $ambildata = mysqli_connect ($sql);
+
+        while ($tampil = mysqli_fetch_array($ambildata)){
+        echo "
+        <tr>
+            <td>$no</td>
+            <td>$tampil[nama]</td>
+            <td>$tampil[nama_siswa]</td>
+        ";
+        }
+
+    ?>
+   </table>
+</body>
+</html>
+
---
+++ edit.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Form</title>
+</head>
+<body bgcolor= grey>
+    <center><h2>EDIT DATA ANDA</h2></center><br>
+    <a href= "tampilan1_1.html">BACK</a>
+
+    
+</body>
+</html>
